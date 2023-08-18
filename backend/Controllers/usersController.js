ea51555b57d493676d8ab04c108c0e13ca325f35
Calvince Ndemo <calyndemo16@gmail.com>
2023-08-18 16:41:10 +0300

    aded

--- backend/Controllers/usersController.js
+++ backend/Controllers/usersController.js
@@ -5,6 +5,7 @@ const { createUsersTable } = require("../Database/Tables/createTables")
 const registerUser = async (req, res)=>{
     try {
         createUsersTable()
+        
     } catch (error) {
         return res.json({Error:error})
     }
