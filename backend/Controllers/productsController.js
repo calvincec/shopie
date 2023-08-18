7af37539fdd0ceb6230b9a978016664f13b53c08
Calvince Ndemo <calyndemo16@gmail.com>
2023-08-18 16:53:59 +0300

    resolving merging

---
+++ backend/Controllers/productsController.js
@@ -0,0 +1,15 @@
+const { createProductsTable } = require("../Database/Tables/createTables")
+
+
+
+
+const newProduct = async (req, res)=>{
+    try {
+        createProductsTable()
+        
+    } catch (error) {
+        return res.json({Error:error})
+    }
+}
+
+
---
+++ backend/Controllers/usersController.js
@@ -0,0 +1,13 @@
+const { createUsersTable } = require("../Database/Tables/createTables")
+
+
+
+const registerUser = async (req, res)=>{
+    try {
+        createUsersTable()
+        
+    } catch (error) {
+        return res.json({Error:error})
+    }
+}
+
